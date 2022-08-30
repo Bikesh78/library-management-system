87a35255cd80962c104d468272ceac3537093a72
Bikesh Rai <bikeshrai78@gmail.com>
2022-08-30 21:39:37 +0545

    Add pie chart

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,61 @@
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const totalRecordData = {
+  labels: [
+    "Total Items",
+    "Total Type",
+    "Items Lost",
+    "Items Damaged",
+    "Items Issued",
+    "Items returned",
+  ],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "#E91CC1",
+        "#63CFCF",
+        "#FD678D",
+        "#E368CB",
+        "#C0B4B2",
+        "#68C3A0",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+export const currentMonthData = {
+  labels: ["Items Added", "Items Issued", "Items Returned"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3],
+      backgroundColor: ["#63CFCF", "#C0B4B2", "#68C3A0"],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 const Dashboard = () => {
   return (
     <div className="main-content dashboard">
@@ -10,6 +66,7 @@ const Dashboard = () => {
           </div>
           <div className="card-body">
             <h2>Library Book Record</h2>
+            <Pie data={totalRecordData} />
           </div>
         </div>
         <div className="card card-medium">
@@ -47,6 +104,7 @@ const Dashboard = () => {
           </div>
           <div className="card-body">
             <h2>Library Book Record</h2>
+            <Pie data={currentMonthData} />
           </div>
         </div>
         <div className="card card-medium">
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const location = useLocation();
-  console.log(location);
   return (
     <div className="sidebar">
       <Link to="/">
